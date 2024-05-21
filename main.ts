c87da74295135ac7837bdcd6859ef12cc04ea83f
StrnadMarek <149717630+StrnadMarek@users.noreply.github.com>
2024-05-21 17:09:26 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
+basic.showIcon(IconNames.Diamond)
+radio.setGroup(210)
 
+basic.forever(function() {
+    radio.sendValue("mgx", input.acceleration(Dimension.X))
+    radio.sendValue("mgy", input.acceleration(Dimension.Y))
+})
