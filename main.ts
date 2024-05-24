3fdb857e64f705bd41ff278ab776f3af4bde35ff
StrnadMarek <149717630+StrnadMarek@users.noreply.github.com>
2024-05-24 11:07:08 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,32 @@
 basic.showIcon(IconNames.Diamond)
 radio.setGroup(210)
 
-basic.forever(function() {
-    radio.sendValue("mgx", input.acceleration(Dimension.X))
-    radio.sendValue("mgy", input.acceleration(Dimension.Y))
+let block: boolean = false
+
+input.onButtonPressed(Button.AB, function() {
+    block = !block
+    if (block) {
+        radio.sendValue("block", 1)
+    } else {
+        radio.sendValue("unblock", 0)
+    }
+})
+/*input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    radio.sendValue("main", 0)
+})*/
+
+input.onButtonPressed(Button.A, function () {
+    radio.sendValue("buttonA", 0)
+})
+
+input.onButtonPressed(Button.B, function () {
+    radio.sendValue("buttonB", 0)
+})
+
+
+basic.forever(function () {
+    if (!block) {
+        radio.sendValue("mgx", input.acceleration(Dimension.X))
+        radio.sendValue("mgy", input.acceleration(Dimension.Y))
+    }
 })
